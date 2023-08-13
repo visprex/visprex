b2ffbc67d9e455f7e12197a28b0c4b766264635b
KengoA <20113339+KengoA@users.noreply.github.com>
2023-08-13 18:12:45 +0100

    lint fix

--- src/components/data/Dataloader.tsx
+++ src/components/data/Dataloader.tsx
@@ -37,7 +37,7 @@ const Dataloader: React.FC<DataloaderProps> = ({ onDataParsed }) => {
 
   return (
     <>
-        <div className="h-auto mb-2">
+        <div className="mb-2 h-auto">
         {
           datasets.map((dataset, idx) => (
             <button
