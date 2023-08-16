c78f63bae2b3dfa2022bea3af55d3a686c0d1df3
KengoA <20113339+KengoA@users.noreply.github.com>
2023-08-16 05:45:10 +0100

    impl stats utils

---
+++ src/utils/stats.ts
@@ -0,0 +1,54 @@
+import { NumberSchema } from './schema';
+
+export type Correlation = {
+    x: string,
+    y: string,
+    coef: number,
+};
+
+export function calculateCorrelations(matrix: any[][], schema: NumberSchema[]): Correlation[] {
+    const nCols = matrix.length;
+    const nRows = matrix[0].length;
+    const correlations: Correlation[] = [];
+    const stds: number[] = [];
+
+    for (let col = 0; col < nCols; col++) {
+        let sum = 0;
+        for (let row = 0; row < nRows; row++) {
+            if (typeof matrix[col][row] === 'number') {
+                const diff = matrix[col][row] - schema[col].mean;
+                sum += diff * diff;
+            }
+        }
+        stds[col] = Math.sqrt(sum);
+    }
+
+    for (let colX = 0; colX < nCols; colX++) {
+        for (let colY = 0; colY < nCols; colY++) {
+            if (colX !== colY) {
+                let covariance = 0;
+                for (let row = 0; row < nRows; row++) {
+                    if (typeof matrix[colX][row] === 'number' && typeof matrix[colY][row] === 'number') {
+                        const diffX = matrix[colX][row] - schema[colX].mean;
+                        const diffY = matrix[colY][row] - schema[colY].mean;
+                        covariance += diffX * diffY;
+                    }
+                }
+                const coef = covariance / (stds[colX] * stds[colY]);
+                correlations.push({
+                    x: schema[colX].key,
+                    y: schema[colY].key,
+                    coef,
+                });
+            } else {
+                correlations.push({
+                    x: schema[colX].key,
+                    y: schema[colY].key,
+                    coef: 1,
+                });
+            }
+        }
+    }
+
+    return correlations;
+}
