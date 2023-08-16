67fea78c95c8d552eac1670056006dbbe72827e0
KengoA <20113339+KengoA@users.noreply.github.com>
2023-08-16 05:46:40 +0100

    refactor utils

--- src/utils/mixpanel.ts
+++
@@ -1,6 +0,0 @@
-import mixpanel from 'mixpanel-browser';
-
-mixpanel.init("ef5a8fd8aa0ff739a4a0be87cdcf3e49", { debug: true, track_pageview: true, persistence: 'localStorage' }); 
-mixpanel.identify("anonymous")
-
-// Track an event. It can be anything, but in this example, we're tracking a Sign Up event.
--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -6,6 +6,16 @@ export function center(array: number[], mean: number): number[] {
     return array.map((value: number) => value - mean);
 }
 
-export function logPlusOneTransform(array: number[]): number[] {
-    return array.map((value: number) => Math.log(value+1));
+export function log10Transform(array: number[]): number[] {
+    return array.map((value: number) => Math.log10(value));
+}
+
+export function logETransform(array: number[]): number[] {
+    return array.map((value: number) => Math.log(value));
+}
+
+export const transformOps:  {[key: string]: (x: number) => number}= {
+    "none": (x: number) => x,
+    "log10(x)": (x: number) => Math.log10(x),
+    "ln(x)": (x: number) => Math.log(x),
 }
