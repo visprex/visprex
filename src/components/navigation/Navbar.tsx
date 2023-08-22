06645058a68b98a7fcb5cc91dd867e320ab3a858
KengoA <20113339+KengoA@users.noreply.github.com>
2023-08-22 02:42:51 +0100

    move logo

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from '../../logo.svg'
+import logo from './logo.svg';
+import { classNames } from '../../utils/classnames';
 
 interface NavBarProps {
 }
 
 const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
   return (
-    <nav className="border-gray-700 bg-gray-800">
-        <div className="mx-auto flex max-w-screen-2xl flex-wrap items-center justify-between p-4">
-            <a href="/" className="ml-5 flex h-7 items-center">
-                <img src={logo} className="mr-3 h-full" alt="Flowbite Logo" />
+    <div className="w-full">
+        <nav className="border-gray-700 bg-gray-800">
+        <div className="mx-auto flex flex-wrap items-center justify-between p-4">
+            <a href="/" className="mx-auto flex flex-col h-7 items-center">
+                <img src={logo} className="mr-3 h-full" alt="Visprex Logo" />
             </a>
-            <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden" aria-controls="navbar-solid-bg" aria-expanded="false">
-                <span className="sr-only">Open main menu</span>
-                <svg className="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button>
-            <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-            </div>
         </div>
-    </nav>
+        </nav>
+    </div>
   );
 };
 
