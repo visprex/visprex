d1178fba59deb3c1b85acf117441c2b2fb5bf55b
KengoA <20113339+KengoA@users.noreply.github.com>
2023-08-22 02:45:03 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState, useEffect, useRef } from 'react'
 import { Histogram }  from './components/graph/Histogram/Histogram';
-import { Scatterplot }  from './components/graph/Scatterplot';
+import { Scatterplot }  from './components/graph/Scatterplot/Scatterplot';
 import { CorrelationMatrix }  from './components/graph/CorrelationMatrix/CorrelationMatrix';
-import { inferNumberSchema, NumberSchema } from './utils/schema';
+import { inferSchema, Schema } from './utils/schema';
 import { classNames } from './utils/classnames';
 import { transpose } from './utils/transform';
 import Navbar from './components/navigation/Navbar';
 import NoDatasetSelected from './components/navigation/NoDatasetSelected';
 import Dataloader from './components/data/Dataloader';
+import DataCard from './components/data/DataCard';
 import { ChartBarIcon, CircleStackIcon, CubeTransparentIcon, CalculatorIcon } from '@heroicons/react/24/outline'
 
 export default function App() {
@@ -40,17 +41,16 @@ export default function App() {
   const [currentTab, setCurrentTab] = useState("Datasets");
   const [matrix, setMatrix] = useState<any[][]>([])
   const [keys, setKeys] = useState<string[]>([])
-  const [schema, setSchema] = useState<NumberSchema[]>([])
+  const [schema, setSchema] = useState<Schema[]>([])
 
   const handleDataParsed = (parsed: Papa.ParseResult<any>) => {
     setKeys(Object.keys(parsed.data[0]))
     setMatrix(transpose(parsed.data.map(Object.values)))
-    setCurrentTab('Histogram')
   };
 
   useEffect(() => {
     if (keys.length > 0 && matrix.length > 0) {
-      setSchema(inferNumberSchema(keys, matrix))
+      setSchema(inferSchema(keys, matrix))
     }
   }, [keys, matrix])
 
@@ -65,9 +65,9 @@ export default function App() {
   };
 
   return (
-    <div ref={elementRef} className='container mx-auto w-full'>        
+    <div className='w-full'>        
       <Navbar/>
-      <div className="mx-auto w-full p-4">
+      <div ref={elementRef} className="container mx-auto p-4">
         <div className="border-b border-gray-200">
           <nav className="flex flex-col space-y-4 sm:flex-row sm:space-x-8 sm:space-y-0" aria-label="Tabs">
             {tabs.map((tab) => (
@@ -98,7 +98,12 @@ export default function App() {
           </nav>
         </div>
         <div>
-          {currentTab === 'Datasets' && <Dataloader onDataParsed={handleDataParsed}/>}
+          {currentTab === 'Datasets' && 
+            <>
+              <Dataloader onDataParsed={handleDataParsed}/>
+              {(schema.length > 0 && <DataCard schema={schema} />)}
+            </>
+          }
           {currentTab === 'Histogram' && (schema.length > 0 ? <Histogram height={450} width={width} matrix={matrix} keys={keys} schema={schema}/> : <NoDatasetSelected/>)}
           {currentTab === 'Scatter Plot' && (schema.length > 0 ? <Scatterplot height={450} width={width} matrix={matrix} keys={keys} schema={schema}/> : <NoDatasetSelected/>)}
           {currentTab === 'Correlation Matrix' && (schema.length > 0 ? <CorrelationMatrix height={450} width={width} matrix={matrix} keys={keys} schema={schema}/> : <NoDatasetSelected/>)}
