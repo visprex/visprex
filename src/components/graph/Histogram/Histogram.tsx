c49f4ac9fa26d8743a1b74af2b1040cefc7377b4
KengoA <20113339+KengoA@users.noreply.github.com>
2023-08-22 02:44:23 +0100

    categorical features

--- src/components/graph/Histogram/Histogram.tsx
+++ src/components/graph/Histogram/Histogram.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import { Schema, NumberSchema, CategoricalSchema, DataType } from "../../../utils/schema";
+import { logETransform, log10Transform, center, TransformType } from "../../../utils/transform";
 import { Renderer } from "./Renderer";
-import { NumberSchema } from "../../../utils/schema";
-import { logETransform, log10Transform, center } from "../../../utils/transform";
 
 const BUTTONS_HEIGHT = 50;
 
@@ -9,7 +9,7 @@ type HistogramProps = {
   width: number;
   height: number;
   matrix: any[][];
-  schema: NumberSchema[],
+  schema: Schema[],
   keys: string[]
 };
 
@@ -21,20 +21,103 @@ export const Histogram = ({
   keys
 }: HistogramProps) => {
   const firstSchemaItem = schema[0];
-  const [selectedData, setSelectedData] = useState(matrix[0]);
+
+  let initialDomain: [number, number];
+  firstSchemaItem.type === DataType.Number ?
+    initialDomain = [(firstSchemaItem as NumberSchema).range.min-1, (firstSchemaItem as NumberSchema).range.max+1] :
+    initialDomain = [0,Object.keys((firstSchemaItem as CategoricalSchema).frequencies).length];
+
+  let initialNumberData: number[];
+  let initialCategoricalData: {[key: string]:number};
+
+  if (firstSchemaItem.type === DataType.Number) {
+    initialNumberData = matrix[0]
+    initialCategoricalData = {}
+  } else {
+    initialNumberData = []
+    initialCategoricalData = (firstSchemaItem as CategoricalSchema).frequencies
+  }
+
+  const [selectedNumberData, setSelectedNumberData] = useState<number[]>(initialNumberData);
+  const [selectedCategoricalData, setSelectedCategoricalData] = useState<{[key: string]:number}>(initialCategoricalData);
+
   const [currentIdx, setCurrentIdx] = useState(0);
-  const [currentMin, setCurrentMin] = useState(firstSchemaItem.range.min);
-  const [currentMax, setCurrentMax] = useState(firstSchemaItem.range.max);
-  const [currentTransform, setCurrentTransform] = useState('none');
+
+  const [currentDomain, setCurrentDomain] = useState<[number, number]>(initialDomain);
+  const [currentDatatype, setCurrentDatatype] = useState(firstSchemaItem.type);
+  
+  const [currentTransform, setCurrentTransform] = useState(TransformType.None);
   const [errorMessage, setErrorMessage] = useState("");
+
+  function handleColumnSelect(idx: number) {
+    setCurrentIdx(idx);
+    const schemaItem = schema[idx];
+    if (schemaItem.type === DataType.Number) {
+      setCurrentDatatype(DataType.Number);
+      setSelectedNumberData(matrix[idx]);
+      setCurrentDomain([schemaItem.range.min-1, schemaItem.range.max+1]);
+    } else {
+      setCurrentDatatype(DataType.Categorical);
+      setSelectedCategoricalData((schemaItem as CategoricalSchema).frequencies);
+      setCurrentDomain([0,Object.keys((schemaItem as CategoricalSchema).frequencies).length]);
+    }
+    setCurrentTransform(TransformType.None);
+  }
+
+  function handleTransform(key: TransformType) {
+    {
+      const currentData = matrix[currentIdx];
+      const schemaItem = schema[currentIdx];
+      setCurrentTransform(key);
+      if (schemaItem.type === DataType.Categorical) {
+        setErrorMessage("");
+        return;
+      }
+      switch (key) {
+        case TransformType.None:
+          setSelectedNumberData(currentData);
+          setCurrentDomain([schemaItem.range.min-1, schemaItem.range.max+1]);
+          setErrorMessage("");
+          break;
+        case TransformType.Ln:
+          if (schemaItem.range.min <= 0) {
+            setErrorMessage("Error: All numbers must be positive for ln(x) transform.");
+            setCurrentTransform(TransformType.None);
+            return;
+          }
+          setSelectedNumberData(logETransform(currentData));
+          setCurrentDomain([Math.log(schemaItem.range.min), Math.log(schemaItem.range.max)]);
+          setErrorMessage("");
+          break;
+        case TransformType.Log10:
+          if (schemaItem.range.min <= 0) {
+            setErrorMessage("Error: All numbers must be positive for log10(x) transform.");
+            setCurrentTransform(TransformType.None);
+            return;
+          }
+          setSelectedNumberData(log10Transform(currentData));
+          setCurrentDomain([Math.log10(schemaItem.range.min), Math.log10(schemaItem.range.max)]);
+          setErrorMessage("");
+          break;
+        case TransformType.Centering:
+          setSelectedNumberData(center(currentData, schemaItem.mean));
+          setCurrentDomain([schemaItem.range.min-schemaItem.mean, schemaItem.range.max-schemaItem.mean]);
+          setErrorMessage("");
+          break;
+        default:
+          break;
+      }
+    }
+  }
 
   return (
     <div id="histogram">
       <Renderer
         width={width}
         height={height - BUTTONS_HEIGHT}
-        domain={[currentMin, currentMax]}
-        data={selectedData}
+        domain={currentDomain}
+        data={currentDatatype === DataType.Number ? selectedNumberData : selectedCategoricalData}
+        datatype={currentDatatype}
       />
       <div>
           <span>Select column: </span>
@@ -48,14 +131,7 @@ export const Histogram = ({
                   ${currentIdx === idx ? 'bg-indigo-500' : 'text-indigo-500'}
                   ${currentIdx === idx ? 'opacity-100' : 'opacity-70'}`
                 }
-                onClick={() => {
-                  setCurrentIdx(idx);
-                  setSelectedData(matrix[idx]);
-                  const schemaItem = schema[idx];
-                  setCurrentMin(schemaItem.range.min-1);
-                  setCurrentMax(schemaItem.range.max+1);
-                  setCurrentTransform('none');
-                }}
+                onClick={() => handleColumnSelect(idx)}
               >
                 {key}
               </button>
@@ -65,7 +141,7 @@ export const Histogram = ({
       <div>
           <span>Transform:</span>
           {
-              ['none', 'mean centering', 'log10(x)', 'ln(x)'].map((key) => (
+              [TransformType.None, TransformType.Centering, TransformType.Log10, TransformType.Ln].map((key) => (
                 <button
                   key={key}
                   className={`
@@ -74,49 +150,7 @@ export const Histogram = ({
                     ${currentTransform === key ? 'bg-indigo-500' : 'text-indigo-500'}
                     ${currentTransform === key ? 'opacity-100' : 'opacity-70'}`
                   }
-                  onClick={() => {
-                    const currentData = matrix[currentIdx];
-                    const schemaItem = schema[currentIdx];
-                    setCurrentTransform(key);
-                    switch (key) {
-                      case "none":
-                        setSelectedData(currentData);
-                        setCurrentMin(schemaItem.range.min-1);
-                        setCurrentMax(schemaItem.range.max+1);
-                        setErrorMessage("");
-                        break;
-                      case "ln(x)":
-                        if (schemaItem.range.min <= 0) {
-                          setErrorMessage("Error: All numbers must be positive for ln(x) transform.");
-                          setCurrentTransform('none');
-                          return;
-                        }
-                        setSelectedData(logETransform(currentData));
-                        setCurrentMin(Math.log(schemaItem.range.min));
-                        setCurrentMax(Math.log(schemaItem.range.max));
-                        setErrorMessage("");
-                        break;
-                      case "log10(x)":
-                        if (schemaItem.range.min <= 0) {
-                          setErrorMessage("Error: All numbers must be positive for log10(x) transform.");
-                          setCurrentTransform('none');
-                          return;
-                        }
-                        setSelectedData(log10Transform(currentData));
-                        setCurrentMin(Math.log10(schemaItem.range.min));
-                        setCurrentMax(Math.log10(schemaItem.range.max));
-                        setErrorMessage("");
-                        break;
-                      case "mean centering":
-                        setSelectedData(center(currentData, schemaItem.mean));
-                        setCurrentMin(schemaItem.range.min-schemaItem.mean);
-                        setCurrentMax(schemaItem.range.max-schemaItem.mean);
-                        setErrorMessage("");
-                        break;
-                      default:
-                        break;
-                    }
-                  }}
+                  onClick={() => handleTransform(key)}
                 >
                   {key}
                 </button>
--- src/components/graph/Histogram/Renderer.tsx
+++ src/components/graph/Histogram/Renderer.tsx
@@ -1,55 +1,90 @@
 import { useEffect, useMemo, useRef } from "react";
 import * as d3 from "d3";
 import { Rectangle } from "./Rectangle";
+import { DataType } from "../../../utils/schema";
 
 const MARGIN = { top: 30, right: 30, bottom: 40, left: 50 };
-
-const BUCKET_NUMBER = 40;
 const BUCKET_PADDING = 2;
+const BUCKET_NUMBER = 40;
 
 type RendererProps = {
   width: number;
   height: number;
-  domain: [number, number]
-  data: number[];
+  domain: [number, number];
+  data: number[] | {[key: string]:number};
+  datatype: DataType;
 };
 
-export const Renderer = ({ width, height, domain, data }: RendererProps) => {
+export const Renderer = ({ width, height, domain, data, datatype }: RendererProps) => {
   const histRef = useRef(null);
   const boundsWidth = width - MARGIN.right - MARGIN.left;
   const boundsHeight = height - MARGIN.top - MARGIN.bottom;
+  
+  const xLabelScale = useMemo(() => {
+    if (datatype === DataType.Number) {
+      return d3.scaleBand()
+    }
+    return d3
+      .scaleBand()
+      .range([0, boundsWidth])
+      .domain(Object.keys(data))
+      .padding(0.2);
+  }, [data, boundsWidth, datatype]);
 
   const xScale = useMemo(() => {
     return d3.scaleLinear().domain(domain).range([10, boundsWidth]);
-  }, [width, domain]);
+  }, [width, domain, datatype]);
 
   const buckets = useMemo(() => {
-    const bucketGenerator = d3
-      .bin()
-      .value((d) => d)
-      .domain(domain)
-      .thresholds(xScale.ticks(BUCKET_NUMBER));
-    return bucketGenerator(data);
-  }, [xScale, domain, data]);
+    if (datatype === DataType.Categorical) {
+        return Object.values(data).map((value: number, index: number) =>
+        ({
+            x0: index,
+            x1: index+1,
+            length: value
+        } as d3.Bin<number, number>))
+    } else {
+        const bucketGenerator = d3
+            .bin()
+            .value((d) => d)
+            .domain(domain)
+            .thresholds(xScale.ticks(BUCKET_NUMBER));
+        return bucketGenerator(data as number[]);
+    }
+  }, [xScale, domain, data, datatype]);
 
   const yScale = useMemo(() => {
-    const max = Math.max(...buckets.map((bucket) => bucket?.length));
+    let max;
+    datatype === DataType.Categorical ?
+        max = Math.max(...Object.values(data)) :
+        max = Math.max(...buckets.map((bucket) => bucket?.length))
     return d3.scaleLinear().range([boundsHeight, 0]).domain([0, max]).nice();
-  }, [data, height, boundsHeight, boundsWidth]);
+  }, [data, height, boundsHeight, boundsWidth, datatype]);
 
   useEffect(() => {
     const svgElement = d3.select(histRef.current);
     svgElement.selectAll("*").remove();
 
-    const xAxisGenerator = d3.axisBottom(xScale);
+    const xAxisGenerator = datatype === DataType.Categorical ? d3.axisBottom(xLabelScale) : d3.axisBottom(xScale);
     svgElement
       .append("g")
       .attr("transform", "translate(0," + boundsHeight + ")")
       .call(xAxisGenerator);
+    if (datatype === DataType.Categorical && Object.keys(data).length > 10) {
+        svgElement
+            .selectAll("text")  
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-45)")
+            .attr("font-size", "9px")
+    }
 
     const yAxisGenerator = d3.axisLeft(yScale);
-    svgElement.append("g").call(yAxisGenerator);
-  }, [xScale, yScale, boundsHeight]);
+    svgElement
+        .append("g")
+        .call(yAxisGenerator);
+  }, [xScale, yScale, xLabelScale, boundsHeight, datatype]);
 
   const allRects = buckets.map((bucket, i) => {
     const { x0, x1 } = bucket;
