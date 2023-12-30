ef9d6592bc1f8cf0f0052c4e740f89710b9cd722
KengoA <20113339+KengoA@users.noreply.github.com>
2023-12-30 12:13:18 +0900

    cleanup static assets

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 
 interface NavBarProps {
 }
@@ -10,7 +9,7 @@ const NavBar: React.FC<NavBarProps> = () => {
         <nav className="border-gray-700 bg-gray-800">
         <div className="mx-auto flex flex-wrap items-center justify-between p-4">
             <a href="/" className="mx-auto flex h-7 flex-col items-center">
-              <img src={logo} className="mr-3 h-auto max-h-full w-auto" alt="Visprex Logo" />
+              <img src="/logo.png" className="mr-3 h-auto max-h-full w-auto" alt="Visprex Logo" />
             </a>
         </div>
         </nav>
