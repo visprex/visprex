95f08297272a5b921812b8877d84d7fc85a287ee
KengoA <20113339+KengoA@users.noreply.github.com>
2023-12-30 12:20:01 +0900

    make the image more responsive

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
-interface NavBarProps {
-}
+interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = () => {
   return (
     <div className="w-full">
-        <nav className="border-gray-700 bg-gray-800">
+      <nav className="border-gray-700 bg-gray-800">
         <div className="mx-auto flex flex-wrap items-center justify-between p-4">
-            <a href="/" className="mx-auto flex h-7 flex-col items-center">
-              <img src="/logo.png" className="mr-3 h-auto max-h-full w-auto" alt="Visprex Logo" />
-            </a>
+          <a href="/" className="mx-auto flex h-7 flex-col items-center">
+            <img
+              src="/logo.png"
+              className="mr-3 h-auto max-h-full w-auto"
+              alt="Visprex Logo"
+              style={{ maxWidth: '100%', height: 'auto' }}
+            />
+          </a>
         </div>
-        </nav>
+      </nav>
     </div>
   );
 };
