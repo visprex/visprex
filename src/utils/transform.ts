bc5564b969913d481eace3eec6bbd8bac3f9989c
Kengo <kengo@hey.com>
2024-10-20 15:50:16 +0100

    Remove mean centering (#13)
    * styling around filters and scatterplot
    
    * remove mean centering

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -1,18 +1,13 @@
 import { Value } from "./schema";
 
 export enum TransformType {
-    None = "none",
-    Centering = "mean centering",
+    None = "x",
     Log10 = "log10(x)",
     Ln = "ln(x)",
 }
 
 export function transpose(matrix: Value[][]): Value[][] {
     return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
-}
-
-export function center(array: number[], mean: number): number[] {
-    return array.map((value: number) => value - mean);
 }
 
 export function log10Transform(array: number[]): number[] {
