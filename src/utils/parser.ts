ad8a8a120bd59fe72eb5f31335c50908f1df4c49
KengoA <20113339+KengoA@users.noreply.github.com>
2024-11-25 02:56:29 +0000

    refactor schema parser logic with DateTime type

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,20 +1,19 @@
-import { DataType, Schema, CategoricalSchema, NumberSchema, Value } from '../types/schema';
+import { DataType, Schema, CategoricalSchema, NumberSchema, DateTimeSchema, Value } from '../types/schema';
+import dayjs from 'dayjs';
 import _ from 'lodash';
 
+function isDatetime(values: Value[]): boolean {
+  return values.every((v) => dayjs(v).isValid())
+}
+
+function isNumerical(values: Value[]): boolean {
+  return !isNaN(_.mean(values))
+}
+
 export function inferSchema(keys: string[], matrix: Value[][]): Schema[] {
- return keys.map((key: string, index: number) => {
-    let schema: Schema;
-    const mean = _.mean(matrix[index])
-    isNaN(mean) ? 
-      schema = {
-        type: DataType.Categorical,
-        rows: matrix[0].length,
-        index,
-        key,
-        frequencies: _.countBy(matrix[index].map((v) => (v === null || v === undefined) ? "N/A" : v))
-      } as CategoricalSchema
-    :
-      schema = {
+  return keys.map((key: string, index: number) => {
+    if (isNumerical(matrix[index])) {
+      return {
         type: DataType.Number,
         rows: matrix[0].length,
         index,
@@ -23,8 +22,29 @@ export function inferSchema(keys: string[], matrix: Value[][]): Schema[] {
           min: _.min(matrix[index]),
           max: _.max(matrix[index]),
         },
-        mean
+        mean: _.mean(matrix[index].filter((v) => v !== null && v !== undefined))
       } as NumberSchema
-    return schema
- })
+    }
+    if (isDatetime(matrix[index])) {
+      const values = matrix[index].map((v) => dayjs(v).unix() * 1000)
+      return {
+        type: DataType.DateTime,
+        rows: matrix[0].length,
+        index,
+        key,
+        range: {
+          min: _.min(values.filter((v) => v > 0)),
+          max: _.max(values.filter((v) => v > 0)),
+        },
+        frequencies: _.countBy(matrix[index].map((v) => (v === null || v === undefined) ? "N/A" : v))
+      } as DateTimeSchema
+    }
+    return {
+      type: DataType.Categorical,
+      rows: matrix[0].length,
+      index,
+      key,
+      frequencies: _.countBy(matrix[index].map((v) => (v === null || v === undefined) ? "N/A" : v))
+    } as CategoricalSchema
+  })
 }
