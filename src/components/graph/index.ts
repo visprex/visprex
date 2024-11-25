d67fddcaba48e81e3880d5eb51af3ec38c5bcf9f
KengoA <20113339+KengoA@users.noreply.github.com>
2024-11-25 03:00:36 +0000

    add Lineplot to index.ts

--- src/components/graph/index.ts
+++ src/components/graph/index.ts
@@ -1,5 +1,6 @@
 import { CorrelationMatrix } from "./CorrelationMatrix/CorrelationMatrix";
 import { Histogram } from "./Histogram/Histogram";
 import { Scatterplot } from "./Scatterplot/Scatterplot";
+import { Lineplot } from "./Lineplot/Lineplot";
 
-export { CorrelationMatrix, Histogram, Scatterplot };
+export { CorrelationMatrix, Histogram, Scatterplot, Lineplot};
