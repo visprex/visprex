5b3b1d2afb54ada72890844336d76ee6cfccafa5
KengoA <20113339+KengoA@users.noreply.github.com>
2025-03-15 18:02:29 +0000

    clean up packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Alias @ to the src folder
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
