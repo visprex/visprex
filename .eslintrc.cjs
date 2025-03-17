78390c847caace14842a44433f1ecf4c617142f7
KengoA <20113339+KengoA@users.noreply.github.com>
2025-03-17 20:20:13 +0000

    configure linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,43 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    es2023: true,
+    node: true,
+    browser: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+    jsxPragma: "react-jsx"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "react-refresh"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-loss-of-precision": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  },
+  ignorePatterns: [
+    "node_modules/",
+    "dist/",
+    "vite.config.ts",
+    ".eslintrc.cjs",
+    "tsconfig.eslint.json"
+  ]
+};
