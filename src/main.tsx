f79dc5349f1f0df5ac2953eb7ebf2a1dab33c89e
Kengo <kengo@hey.com>
2025-05-12 22:57:16 +0100

    feat: Add an option to install as a Progressive Web App (PWA) (#62)
    * add new icon assets and manifest.json
    
    * implement pwa
    
    * bump

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/src/service-worker.js").catch((error) => {
+      console.error("Service Worker registration failed:", error);
+    });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
