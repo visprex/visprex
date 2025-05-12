95b80c66a7d075db20a34104e5f67e00813a044d
KengoA <20113339+KengoA@users.noreply.github.com>
2025-05-12 22:53:24 +0100

    implement pwa

---
+++ src/service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "visprex-cache-v1";
+
+const urlsToCache = [
+  "/", 
+  "/index.html", 
+  "/manifest.json", 
+  "/icon.webp", 
+  "/192.png", 
+  "/512.png",
+  "/src/main.tsx",
+  "/src/index.css",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
